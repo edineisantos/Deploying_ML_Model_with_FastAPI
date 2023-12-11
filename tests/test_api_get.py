1dd6a9615549cfbfdf9820165d47bdeaf433d145
Edinei Santos <7681342+edineisantos@users.noreply.github.com>
2023-12-11 11:42:23 -0300

    Add unit test for the GET method

---
+++ tests/test_api_get.py
@@ -0,0 +1,11 @@
+import pytest
+from httpx import AsyncClient
+from main import app
+
+@pytest.mark.asyncio
+async def test_get_method():
+    async with AsyncClient(app=app, base_url="http://127.0.0.1:8890") as ac:
+        response = await ac.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"greeting": 
+    "Welcome to the Census Income Prediction API!"}
